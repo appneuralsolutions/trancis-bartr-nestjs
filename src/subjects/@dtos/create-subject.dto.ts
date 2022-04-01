84043ad41fc68f3b5d9f82480588ac060868914f
Nikhil Panhcal <npanchal410@gmail.com>
2022-04-01 19:11:04 +0530

    Dto updated

--- src/subjects/@dtos/create-subject.dto.ts
+++ src/subjects/@dtos/create-subject.dto.ts
@@ -1,12 +1,11 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateSubjectDto {
-    user_Id: string
-    
-    @IsNotEmpty()
-    subject: string;
+  
+  user_Id: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  subject: string;
 }
-
-
-
-
-
