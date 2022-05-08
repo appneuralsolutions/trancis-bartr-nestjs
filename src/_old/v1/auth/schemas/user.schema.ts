d462428a694f4c3ed5dd0f72a4eb0813d28fa60f
Ajay Prajapat <ajayprajapat@live.com>
2022-05-08 13:29:05 +0530

    fixed

--- src/_old/v1/auth/schemas/user.schema.ts
+++ src/_old/v1/auth/schemas/user.schema.ts
@@ -5,7 +5,7 @@ export const UserSchema = new mongoose.Schema({
   firstName: { type: String, require: true },
   lastName: { type: String },
   email: { type: String, unique: true, require: true },
-  uname: { type: String, require: true },
+  uname: { type: String, require: true, unique: true },
   phone: { type: String, unique: true, require: true },
   userType: { type: String, require: true },
   // empCode: { type: String, unique: true, require: true },
