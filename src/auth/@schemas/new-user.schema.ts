a9a67e7f1f0bf3c79b08b879571dcddefc3acfda
Ajay Prajapat <ajayprajapat@live.com>
2022-05-15 17:31:20 +0530

    fixed

--- src/auth/@schemas/new-user.schema.ts
+++ src/auth/@schemas/new-user.schema.ts
@@ -9,4 +9,4 @@ export const NewUserSchema = new mongoose.Schema({
   location: String,
   password: String,
   photo: String,
-});
+}).set('timestamps', true);
--- src/feedback/@schema/feedback.schema.ts
+++ src/feedback/@schema/feedback.schema.ts
@@ -4,4 +4,4 @@ export const FeedbackSchema = new mongoose.Schema({
   email: String,
   message: String,
   rating: String,
-});
+}).set('timestamps', true);
