e2b0d8bae980f6a834f6af5eed73fb94c1686c29
kailash.vcraft <kailash.vcraft@gmail.com>
2022-05-20 21:09:20 +0530

    user registration and login issue resolved

--- src/auth/@schemas/user.schema.ts
+++ src/auth/@schemas/user.schema.ts
@@ -52,26 +52,47 @@ export const UserSchema = new mongoose.Schema({
   modifiedBy: String,
 }).set('timestamps', true);
 
-UserSchema.pre('save', async (next) => {
-  console.log(this);
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
+UserSchema.pre('save', function (next) {
   const user: any = this;
-  // if (!user.isModified('password')) {
-  //   return next();
-  // }
-  await bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(user?.password, salt, (err1, hash) => {
-      if (err1) {
-        return next(err1);
-      }
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
       user.password = hash;
       next();
     });
   });
-}).set('timestamps', true);
+});
+
+// UserSchema.pre('save', async (next) => {
+//   console.log(this);
+//   // eslint-disable-next-line @typescript-eslint/no-this-alias
+//   const user: any = this;
+//   // if (!user.isModified('password')) {
+//   //   return next();
+//   // }
+//    bcrypt.genSalt(10, (err, salt) => {
+//      console.log('bcrypt is called')
+//     if (err) {
+//       return next(err);
+//     }
+//     bcrypt.hash(user?.password, salt, (err1, hash) => {
+//       if (err1) {
+//         return next(err1);
+//       }
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// }).set('timestamps', true);
 
 // UserSchema.methods.checkPassword = function (attempt, callback) {
 //   const user: any = this;
