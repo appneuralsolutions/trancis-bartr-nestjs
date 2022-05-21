98d63b5034eda5c39b64d3d79d5d3b52a39ff2ad
ashwinsharoof <ashwinsharoof01@gmail.com>
2022-05-21 16:27:01 +0530

    ebay api

--- src/ebay-intg/ebay-intg.controller.ts
+++ src/ebay-intg/ebay-intg.controller.ts
@@ -1,11 +1,28 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Me } from 'src/me/@decorators/me.decorator';
+import { ErrorMessage } from 'src/shared/@constants/error.constant';
+import { Message } from 'src/shared/@constants/messages.constant';
+import { ResponseError, ResponseSuccess } from 'src/shared/@dtos/response.dto';
 
 @Controller('ebay-intg')
 export class EbayIntgController {
-  constructor(private httpService: HttpService) {
-    httpService.get('https://api.publicapis.org/entries').subscribe((data) => {
+    
+    constructor(private httpService: HttpService, private jwtService: JwtService) {
+    
+  }
+  @Get()
+  async findByProfile(@Me() me: string): Promise<any> {
+    const userPayload: any = this.jwtService.decode(me);
+
+    const ebaycard = this.httpService.get('https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=3').subscribe((data) => {
       console.log(data);
-    });
+    });;
+    if (ebaycard) {
+      return ebaycard ;
+    } else {
+      return "not able to fetch";
+    }
   }
 }
--- src/ebay-intg/ebay-intg.module.ts
+++ src/ebay-intg/ebay-intg.module.ts
@@ -1,9 +1,10 @@
 import { SharedModule } from 'src/shared/shared.module';
 import { Module } from '@nestjs/common';
 import { EbayIntgController } from './ebay-intg.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, AuthModule],
   controllers: [EbayIntgController],
 })
 export class EbayIntgModule {}
