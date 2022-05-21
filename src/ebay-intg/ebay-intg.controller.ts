eb49400ba506db257aa248f35782e892ec1dc2c8
Ajay Prajapat <ajayprajapat@live.com>
2022-05-21 15:39:48 +0530

    added http call

--- src/ebay-intg/ebay-intg.controller.ts
+++ src/ebay-intg/ebay-intg.controller.ts
@@ -1,4 +1,11 @@
+import { HttpService } from '@nestjs/axios';
 import { Controller } from '@nestjs/common';
 
 @Controller('ebay-intg')
-export class EbayIntgController {}
+export class EbayIntgController {
+  constructor(private httpService: HttpService) {
+    httpService.get('https://api.publicapis.org/entries').subscribe((data) => {
+      console.log(data);
+    });
+  }
+}
--- src/ebay-intg/ebay-intg.module.ts
+++ src/ebay-intg/ebay-intg.module.ts
@@ -1,7 +1,9 @@
+import { SharedModule } from 'src/shared/shared.module';
 import { Module } from '@nestjs/common';
 import { EbayIntgController } from './ebay-intg.controller';
 
 @Module({
+  imports: [SharedModule],
   controllers: [EbayIntgController],
 })
 export class EbayIntgModule {}
