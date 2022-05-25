23a720d79114e0fc16456c6a3256f25b7a3e3695
Ajay Prajapat <ajayprajapat@live.com>
2022-05-25 15:20:44 +0530

    fixed

--- src/feeds/feeds.controller.ts
+++ src/feeds/feeds.controller.ts
@@ -1,11 +1,12 @@
 import { FeedsService } from './feeds.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { IResponse } from 'src/shared/@interfaces/response.interface';
 import { ResponseError, ResponseSuccess } from 'src/shared/@dtos/response.dto';
 import { Message } from 'src/shared/@constants/messages.constant';
 import { ErrorMessage } from 'src/shared/@constants/error.constant';
 import { CreateCard } from 'src/cards/@interface/card.interface';
 import { JwtService } from '@nestjs/jwt';
+import { query } from 'express';
 
 @Controller('feeds')
 export class FeedsController {
@@ -15,8 +16,10 @@ export class FeedsController {
   ) {}
 
   @Get()
-  async findAll(): Promise<IResponse | CreateCard[]> {
-    const feed = await this.feedsService.aggregateFeed();
+  async findAll(
+    @Query('categories') categories: string,
+  ): Promise<IResponse | CreateCard[]> {
+    const feed = await this.feedsService.aggregateFeed(categories);
     if (feed) {
       return new ResponseSuccess(Message.SUCCESSFULLY_FIND_ALL_CARDS, {
         feed,
