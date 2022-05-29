8611989468224c47e6a0096aee36050e6e95223a
ashwinsharoof <ashwinsharoof01@gmail.com>
2022-05-29 11:58:21 -0700

    added ebay api for search query and category api

--- src/ebay-intg/ebay-intg.controller.ts
+++ src/ebay-intg/ebay-intg.controller.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Me } from 'src/me/@decorators/me.decorator';
 
@@ -18,20 +18,32 @@ export class EbayIntgController {
     //   });
   }
   @Get()
-  async findByProfile(@Me() me: string): Promise<any> {
-    // const userPayload: any = this.jwtService.decode(me);
-
-    const ebaycard = this.httpService
+  async findByProfile(@Body() query: Object): Promise<any> {
+      let query_value = Object.values(query)
+      return new Promise((resolve) => {
+        this.httpService
       .get(
-        'https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=3',
+        `https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=${query_value}`,
       )
       .subscribe((data) => {
-        console.log(data);
+        //let result = JSON.stringify(data) comparing price, query
+        resolve(data.data);
+        console.log(data)
       });
-    if (ebaycard) {
-      return ebaycard;
-    } else {
-      return 'not able to fetch';
     }
-  }
+    )}
+    @Post('bycategory')
+    async findbyquery(@Body() sort: Object,@Body() category_ids: Object): Promise<any> {
+      let sort_value = Object.values(sort)
+      let category = Object.values(category_ids)
+      console.log(category)
+      return new Promise((resolve) =>{
+        this.httpService.get(`https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?category_ids=${category[0]}&sort=${category[1]}`)
+    
+        .subscribe((data) => {
+          resolve(data.data);
+          //console.log(data)
+        })
+      })
+    }
 }
