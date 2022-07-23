3a778fc33b752bffc2721266baa024053ba92261
ashwinsharoof <ashwinsharoof01@gmail.com>
2022-07-23 07:03:04 -0700

    added chat api

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,16 +19,23 @@ import { SharedModule } from 'src/shared/shared.module';
 import { SubjectsModule } from './subjects/subjects.module';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { join } from 'path';
+import { MessagesController } from './chat/controllers/messages/messages.controller';
+import { RoomsController } from './chat/controllers/rooms/rooms.controller';
+import { MessagesGateway } from './chat/gateways/messages/message.gateway';
+import { Message } from './chat//models/message.model';
+import { Room } from './chat/models/room.model';
+import { User } from './chat/models/user.model';
 
 @Module({
   imports: [
     SharedModule,
     AppRoutingModule,
-    MongooseModule.forRoot('mongodb://64.227.166.146:28018/bartr-v1', {
+    MongooseModule.forRoot('mongodb://64.227.166.146:25290/bartr-v1', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     }),
-    TypegooseModule.forRoot('mongodb://64.227.166.146:28018/bartr-v1', {}),
+    TypegooseModule.forRoot('mongodb://64.227.166.146:25290/bartr-v1', {}),
+    TypegooseModule.forFeature([Message, Room, User]),
     ServeStaticModule.forRoot({
       serveRoot: '/',
       rootPath: join(__dirname, '../www'),
@@ -48,8 +55,8 @@ import { join } from 'path';
     ChatModule,
     FeedsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MessagesController, RoomsController],
+  providers: [AppService, MessagesGateway],
 })
 export class AppModule {
   // configure(consumer: MiddlewareConsumer) {
