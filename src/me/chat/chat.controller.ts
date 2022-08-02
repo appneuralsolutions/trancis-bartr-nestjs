f4d043c2b8c58361414652bf4c7086099cbf0852
AjayPrajapat <ajayprajapat@live.com>
2022-08-02 15:40:55 +0530

    uuid added

--- src/me/chat/chat.controller.ts
+++ src/me/chat/chat.controller.ts
@@ -5,67 +5,55 @@ import { Chat } from './interface/chat.interface';
 
 @Controller('chat')
 export class ChatController {
-    constructor(
-        private readonly chatService: ChatService,
-      ) {}
+  constructor(private readonly chatService: ChatService) {}
 
-      @Post()
-      async create(
-        @Body() chatDto: CreateChatDto,
-      ): Promise<any> {
-        const data = await this.chatService.create(
-            chatDto
-        );
-        if (data) {
-          return data
-        } else {
-          return "Not Created"
-        }
-      }
-      @Get(':id')
-      async findMy(@Param('id') id: string): // @Me() me: string
-      
-      Promise<any> {
-       
-        //const userPayload: any = this.jwtService.decode(me);
-        const data = await this.chatService.findMy(id);
-        if (data) {
-          return data;
-        } else {
-          return "not able to fetch"
-        }
-      }
-      @Get(':user')
-      async findAll(@Param('user') user: string): // @Me() me: string
-      
-      Promise<any> {
-       
-        //const userPayload: any = this.jwtService.decode(me);
-        console.log(user)
-        const data = await this.chatService.findAll(user);
-        if (data) {
-          return data;
-        } else {
-          return "not able to fetch"
-        }
-      }
-    
-      @Put(':id')
-      async update(
-        @Body() createFeedbackDto: CreateChatDto,
-        @Param('id') id: string
-        //@Me() me: string,
-      ): Promise<any> {
-        //const userPayload: any = this.jwtService.decode(me);
-        const data = await this.chatService.update(
-          id,
-          createFeedbackDto,
-        );
-        if (data) {
-          return data
-        } else {
-            return "not updated"
-        }
-      }
+  @Post()
+  async create(@Body() chatDto: CreateChatDto): Promise<any> {
+    const data = await this.chatService.create(chatDto);
+    if (data) {
+      return data;
+    } else {
+      return 'Not Created';
+    }
+  }
+  @Get(':id')
+  async findMy(@Param('id') id: string): // @Me() me: string
 
+  Promise<any> {
+    //const userPayload: any = this.jwtService.decode(me);
+    const data = await this.chatService.findMy(id);
+    if (data) {
+      return data;
+    } else {
+      return 'not able to fetch';
+    }
+  }
+  @Get(':uuid')
+  async findAll(@Param('uuid') uuid: string): // @Me() me: string
+
+  Promise<any> {
+    //const userPayload: any = this.jwtService.decode(me);
+    console.log(uuid);
+    const data = await this.chatService.findAll(uuid);
+    if (data) {
+      return data;
+    } else {
+      return 'not able to fetch';
+    }
+  }
+
+  @Put(':id')
+  async update(
+    @Body() createFeedbackDto: CreateChatDto,
+    @Param('id') id: string,
+    //@Me() me: string,
+  ): Promise<any> {
+    //const userPayload: any = this.jwtService.decode(me);
+    const data = await this.chatService.update(id, createFeedbackDto);
+    if (data) {
+      return data;
+    } else {
+      return 'not updated';
+    }
+  }
 }
--- src/me/chat/chat.service.ts
+++ src/me/chat/chat.service.ts
@@ -6,47 +6,38 @@ import { CreateChatDto } from './dto/chat.dto';
 
 @Injectable()
 export class ChatService {
-    constructor(
-        @InjectModel('Chat') private readonly chatModel: Model<Chat>,
-      ) {}
+  constructor(@InjectModel('Chat') private readonly chatModel: Model<Chat>) {}
 
-      async create(data: CreateChatDto): Promise<Chat> {
-        //data.createdBy = userPayload.userId;
-        const createdData = await new this.chatModel(data).save();
-        return new Promise((resolve) => {
-          resolve(createdData);
-        });
-      }
+  async create(data: CreateChatDto): Promise<Chat> {
+    //data.createdBy = userPayload.userId;
+    const createdData = await new this.chatModel(data).save();
+    return new Promise((resolve) => {
+      resolve(createdData);
+    });
+  }
 
-      async findMy(id): Promise<Chat[]> {
-        //const me = userPayload.userId;
-        const data = await this.chatModel.find({  _id: id});
-        return new Promise((resolve) => {
-          resolve(data);
-        });
-      }
+  async findMy(id): Promise<Chat[]> {
+    //const me = userPayload.userId;
+    const data = await this.chatModel.find({ _id: id });
+    return new Promise((resolve) => {
+      resolve(data);
+    });
+  }
 
-      async findAll(user): Promise<Chat[]> {
-        //const me = userPayload.userId;
-        const data = await this.chatModel.find({ user: user});
-        return new Promise((resolve) => {
-          resolve(data);
-        });
-      }
-    
-      async update(
-        id: string,
-        data: CreateChatDto,
-      ): Promise<Chat> {
-        const feedback = await this.chatModel.findOneAndUpdate(
-          { _id: id},
-          data,
-          {
-            new: true,
-          },
-        );
-        return new Promise((resolve) => {
-          resolve(feedback);
-        });
-      }
+  async findAll(uuid): Promise<Chat[]> {
+    //const me = userPayload.userId;
+    const data = await this.chatModel.find({ uuid: uuid });
+    return new Promise((resolve) => {
+      resolve(data);
+    });
+  }
+
+  async update(id: string, data: CreateChatDto): Promise<Chat> {
+    const feedback = await this.chatModel.findOneAndUpdate({ _id: id }, data, {
+      new: true,
+    });
+    return new Promise((resolve) => {
+      resolve(feedback);
+    });
+  }
 }
