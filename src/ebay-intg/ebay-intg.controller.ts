a305fd07e8c1615f4164c299801a84718161aa62
Ashwinajackus <ashwin.rajaram@ajackus.com>
2022-10-23 23:07:00 +0530

    added ebay token

--- src/ebay-intg/ebay-intg.controller.ts
+++ src/ebay-intg/ebay-intg.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { EbayAuthToken } from 'ebay-oauth-nodejs-client';
 import { ApiTags } from '@nestjs/swagger';
+import eBayApi from 'ebay-api';
 
 @ApiTags('Ebay')
 @Controller('ebay-intg')
@@ -10,59 +11,64 @@ export class EbayIntgController {
   constructor(
     private httpService: HttpService,
     private jwtService: JwtService,
-  ) {
-    // this.httpService
-    //   .get(
-    //     'https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=3',
-    //   )
-    //   .subscribe((data) => {
-    //     console.log(data);
-    //   });
-  }
-
-  @Get()
-  async findByProfile(@Body() query: Object): Promise<any> {
-    const ebayAuthToken = new EbayAuthToken({
-      clientId: 'ashwinR-bartar-SBX-fb454faf4-216e98aa',
-      clientSecret: 'SBX-b454faf41f41-b179-40bd-8e48-09b4',
-      redirectUri: 'ashwin_R-ashwinR-bartar--oaikmz',
+  ) { }
+  @Get('/getToken')
+  async findByItems(@Body() query: Object): Promise<any> {
+    const eBay: any = new eBayApi({
+      appId: 'ashwinR-bartar-PRD-3b44d57bd-5cc21870',
+      certId: 'PRD-b44d57bd4ec7-67da-4ca5-82f8-0b9b',
+      sandbox: false,
+      autoRefreshToken: true
     });
-
-    (async () => {
-      const token = await ebayAuthToken.getApplicationToken('Production');
-      console.log(token);
-    })();
-    const query_value = Object.values(query);
-    return new Promise((resolve) => {
-      this.httpService
-        .get(
-          `https://api.ebay.com/buy/browse/v1/item_summary/search?q=${query_value}`,
-        )
-        .subscribe((data) => {
-          //let result = JSON.stringify(data) comparing price, query
-          resolve(data.data);
-          console.log(data);
-        });
+    eBay.OAuth2.on('refreshAuthToken', (token) => {
+      console.log(token)
+      // Store this token in DB
     });
   }
-  @Post('bycategory')
-  async findbyquery(
-    @Body() sort: Object,
-    @Body() category_ids: Object,
-  ): Promise<any> {
-    const sort_value = Object.values(sort);
-    const category = Object.values(category_ids);
-    console.log(category);
-    return new Promise((resolve) => {
-      this.httpService
-        .get(
-          `https://api.ebay.com/buy/browse/v1/item_summary/search?category_ids=${category[0]}&sort=${category[1]}`,
-        )
 
-        .subscribe((data) => {
-          resolve(data.data);
-          //console.log(data)
-        });
-    });
-  }
+  // @Get()
+  // async findByProfile(@Body() query: Object): Promise<any> {
+  //   const ebayAuthToken = new EbayAuthToken({
+  //     clientId: 'ashwinR-bartar-SBX-fb454faf4-216e98aa',
+  //     clientSecret: 'SBX-b454faf41f41-b179-40bd-8e48-09b4',
+  //     redirectUri: 'ashwin_R-ashwinR-bartar--oaikmz',
+  //   });
+
+  //   (async () => {
+  //     const token = await ebayAuthToken.getApplicationToken('Production');
+  //     console.log(token);
+  //   })();
+  //   const query_value = Object.values(query);
+  //   return new Promise((resolve) => {
+  //     this.httpService
+  //       .get(
+  //         `https://api.ebay.com/buy/browse/v1/item_summary/search?q=${query_value}`,
+  //       )
+  //       .subscribe((data) => {
+  //         //let result = JSON.stringify(data) comparing price, query
+  //         resolve(data.data);
+  //         console.log(data);
+  //       });
+  //   });
+  // }
+  // @Post('bycategory')
+  // async findbyquery(
+  //   @Body() sort: Object,
+  //   @Body() category_ids: Object,
+  // ): Promise<any> {
+  //   const sort_value = Object.values(sort);
+  //   const category = Object.values(category_ids);
+  //   console.log(category);
+  //   return new Promise((resolve) => {
+  //     this.httpService
+  //       .get(
+  //         `https://api.ebay.com/buy/browse/v1/item_summary/search?category_ids=${category[0]}&sort=${category[1]}`,
+  //       )
+
+  //       .subscribe((data) => {
+  //         resolve(data.data);
+  //         //console.log(data)
+  //       });
+  //   });
+  // }
 }
